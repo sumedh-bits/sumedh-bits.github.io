676a919ad942c0ae5a28f3e2feaef1ef02785a1a
Rock <f20240801@goa.bits-pilani.ac.in>
2025-01-12 11:08:59 +0530

    ui changes

--- assets/images/json-generator.py
+++ assets/images/json-generator.py
@@ -1,14 +1,13 @@
 from PIL import Image, ExifTags
 import glob
 import json
-images = glob.glob('*.jpg')
+images = glob.glob('full/*.jpg')
 mainjson = {}
 mainjson["images"] = []
 somearray = []
 for image in images:
     img = Image.open(image)
     img_exif = img._getexif()
-    
     shutterspeed = 1/img_exif.get(33434)
     iso = img_exif.get(34866)
     aperture = img_exif.get(33437)
