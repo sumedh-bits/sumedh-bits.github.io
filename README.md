236bcf51f6f4540871cbbf8536d56720c0986b1e
Sumedh <f20240801@goa.bits-pilani.ac.in>
2025-12-17 21:31:11 +0530

    Updated the photography page
    Added many new images
    processor.py rewritten to easily return the json element to be added for a new pic

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This repo hosts my [personal photography page](https://sumedh-bits.github.io/) as well the [Cat-alogue](https://sumedh-bits.github.io/cat-alogue/) which is under development.
+This repo hosts my [personal photography page](https://sumedh-bits.github.io/) as well the [Cat-alogue](https://sumedh-bits.github.io/cat-alogue/).
--- assets/images/processor.py
+++ assets/images/processor.py
@@ -1,15 +1,27 @@
 from PIL import Image
 import json
+import sys
 
+# filename = "20240915_113942.jpg"
+# path = "full/" + filename
+# img = Image.open(path)
+# img_exif = img._getexif()
+# print(img_exif)
+# shutterspeed = 1/img_exif.get(33434)
+# iso = img_exif.get(34866)
+# aperture = img_exif.get(33437)
+# json_child = {"name": filename, "group": "", "description": "", "dimensions": str(img.width) + "-" + str(img.height), "settings": "1/"+ str(shutterspeed) + "s ISO" + str(iso) + " f/" + str(aperture), "location": ""}
+# #img.save("thumb/" + filename, quality=100)
+# print(json.dumps(json_child, indent=4))
 
-filename = "20240915_113942.jpg"
-path = "full/" + filename
-img = Image.open(path)
-img_exif = img._getexif()
-print(img_exif)
-shutterspeed = 1/img_exif.get(33434)
-iso = img_exif.get(34866)
-aperture = img_exif.get(33437)
-json_child = {"name": filename, "group": "", "description": "", "dimensions": str(img.width) + "-" + str(img.height), "settings": "1/"+ str(shutterspeed) + "s ISO" + str(iso) + " f/" + str(aperture), "location": ""}
-#img.save("thumb/" + filename, quality=100)
-print(json.dumps(json_child, indent=4))
+filenames = sys.argv[1:]
+for filename in filenames:
+    img = Image.open("full/" + filename)
+    img.thumbnail((3000, 3000))
+    img.save("thumb/" + filename)
+    img_exif = img._getexif()
+    shutterspeed = 1/img_exif.get(33434)
+    iso = img_exif.get(34866)
+    aperture = img_exif.get(33437)
+    json_child = {"name": filename, "group": "", "description": "", "dimensions": str(img.width) + "-" + str(img.height), "settings": "1/"+ str(shutterspeed) + "s ISO" + str(iso) + " f/" + str(aperture), "location": ""}
+    print(json.dumps(json_child, indent=4))
